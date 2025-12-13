d2400c881f07bda6133078075b27a074740defd3
wisniewsky00 <krystian.wisniewski2906@gmail.com>
2025-12-13 10:08:41 +0100

    chore: add energy mix types and API client

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export interface EnergyMixDay {
+  from: string;
+  to: string;
+  avgGas: number;
+  avgCoal: number;
+  avgBiomass: number;
+  avgNuclear: number;
+  avgHydro: number;
+  avgImports: number;
+  avgOther: number;
+  avgWind: number;
+  avgSolar: number;
+  avgCleanEnergy: number;
+}
